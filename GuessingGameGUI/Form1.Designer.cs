6a3a7b5f7567557f970d723cceb0280009146440
Felipe Woods <wof8317@gmail.com>
2021-12-13 14:41:58 -0600

    Add project files.

---
+++ GuessingGameGUI/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace GuessingGameGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtGuessBox = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Location = new System.Drawing.Point(126, 344);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(181, 60);
+            this.btnPlayAgain.TabIndex = 0;
+            this.btnPlayAgain.Text = "Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(313, 344);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(181, 60);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "Guess!";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(500, 344);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(181, 60);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtGuessBox
+            // 
+            this.txtGuessBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuessBox.Location = new System.Drawing.Point(349, 257);
+            this.txtGuessBox.Name = "txtGuessBox";
+            this.txtGuessBox.Size = new System.Drawing.Size(101, 24);
+            this.txtGuessBox.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(199, 257);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(112, 18);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Insert Text Here";
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(249, 182);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(315, 24);
+            this.lblInstructions.TabIndex = 5;
+            this.lblInstructions.Text = "Guess a number between 1 and 100";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(256, 100);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(298, 42);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Guessing Game!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtGuessBox);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Name = "Form1";
+            this.Text = "GuessingGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtGuessBox;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ GuessingGameGUI/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGameGUI
+{
+    public partial class Form1 : Form
+    {
+        // Initialize variables
+        const int max = 100;
+        const int min = 1;
+        int randomNumber;
+        public Form1()
+        {
+            InitializeComponent();
+            // Generate initial random number from given range.
+            Random rand = new Random();
+            randomNumber = rand.Next(min, max);
+            // The label uses placeholder text for easier viewing in design view in VS.
+            // This clears the placeholder text while the program starts.
+            lblResult.Text = "";
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            
+            int guess;
+
+            guess = Convert.ToInt32(txtGuessBox.Text);
+
+            if (guess != randomNumber)
+            {
+                if (guess < randomNumber)
+                {
+                    // Guess lower than the generated number.
+                    lblResult.Text = "Guess Higher...";
+                }
+                else if (guess > randomNumber)
+                {
+                    // Guess higher than generated number.
+                    lblResult.Text = "Guess Lower...";
+                }
+            }
+            else if (guess == randomNumber)
+            {
+                lblResult.Text = "CORRECT!";
+            }
+
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            // Restarts the random number generator to generate a new number within the given range
+            Random rand = new Random();
+            randomNumber = rand.Next(min, max);
+            txtGuessBox.Clear();
+            lblResult.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
